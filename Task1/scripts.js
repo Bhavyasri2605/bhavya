b98bfd431e4e370236608d8024c90288289efaa7
Bhavyasri2605 <mulebhavyasri26@gmail.com>
2024-07-20 12:34:02 +0530

    Create scripts.js

---
+++ Task1/scripts.js
@@ -0,0 +1,7 @@
+javascript
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted successfully!');
+});
+
+           
